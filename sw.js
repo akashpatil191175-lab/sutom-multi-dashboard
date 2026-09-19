09c3467ebc22ff093ea83289844bfbd014e04377
akashpatil191175-lab <akashpatil191175@gmail.com>
2026-09-19 07:13:17 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'sutom-v1';
+const CACHE_NAME = 'sutom-v2';
+
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -13,6 +14,7 @@ self.addEventListener('install', (e) => {
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
+
   self.skipWaiting();
 });
 
@@ -28,6 +30,7 @@ self.addEventListener('activate', (e) => {
       );
     })
   );
+
   self.clients.claim();
 });
 
